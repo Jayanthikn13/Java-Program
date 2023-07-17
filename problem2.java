2ebfcfc3b1df7369a0c93b3f3523e6775a5c91f5
Jayanthikn13 <129248753+Jayanthikn13@users.noreply.github.com>
2023-07-17 22:01:06 +0530

    Update problem2.java

--- problem2.java
+++ problem2.java
@@ -4,15 +4,14 @@ class NumberSeries {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the starting number (a): ");
-        int a = scanner.nextInt();
-
         System.out.print("Enter the target number (x): ");
         int x = scanner.nextInt();
 
+        int a = 1;
+
         while (a <= x) {
             System.out.print(a + " ");
-            a++;
+            a += 2;
         }
 
         scanner.close();
